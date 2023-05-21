6d7e5457128acc2e07e7884a8b77390602eb631f
luis <luis@LAPTOP-ST2KMDNG>
2023-05-21 17:40:50 -0500

    Agregar archivos de proyecto.

---
+++ AppWebLogisticaMoreno/Controllers/MOrdenCompraController.cs
@@ -0,0 +1,75 @@
+﻿using AppWebLogisticaMoreno.Models.Entidades;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppWebLogisticaMoreno.Controllers
+
+{
+    public class MOrdenCompraController : Controller
+    {
+        public List<MOrdenCompra> GetDatosOrdenCompra()
+        {
+            var listado = new List<MOrdenCompra>();
+            listado.Add(new MOrdenCompra
+            {
+                idOrdenCompra = 1,
+                Proveedor = "Maestro",
+                Descripcion = "Permo de 1/2 x 3/4",
+                UnidadMedida = "Und",
+                Cantidad = 10,
+                Precio = 5,
+
+
+            });
+            listado.Add(new MOrdenCompra
+            {
+                idOrdenCompra = 2,
+                Proveedor = "Sodimac",
+                Descripcion = "Permo de 1/2 x 3/4",
+                UnidadMedida = "Und",
+                Cantidad = 10,
+                Precio = 5,
+            });
+            listado.Add(new MOrdenCompra
+            {
+                idOrdenCompra = 3,
+                Proveedor = "Redondos",
+                Descripcion = "Permo de 1/2 x 3/4",
+                UnidadMedida = "Und",
+                Cantidad = 10,
+                Precio = 5,
+            });
+            listado.Add(new MOrdenCompra
+            {
+                idOrdenCompra = 4,
+                Proveedor = "Maestro",
+                Descripcion = "Permo de 1/2 x 3/4",
+                UnidadMedida = "Und",
+                Cantidad = 10,
+                Precio = 5,
+            });
+            listado.Add(new MOrdenCompra
+            {
+                idOrdenCompra = 5,
+                Proveedor = "Sodimac",
+                Descripcion = "Permo de 1/2 x 3/4",
+                UnidadMedida = "Und",
+                Cantidad = 10,
+                Precio = 5,
+            });
+            listado.Add(new MOrdenCompra
+            {
+                idOrdenCompra = 6,
+                Proveedor = "Maestro",
+                Descripcion = "Permo de 1/2 x 3/4",
+                UnidadMedida = "Und",
+                Cantidad = 10,
+                Precio = 5,
+            });
+            return listado;
+        }
+        public IActionResult Index()
+        { var listado =GetDatosOrdenCompra();
+            return View(listado);
+        }
+    }
+}
---
+++ AppWebLogisticaMoreno/Models/Entidades/MOrdenCompra.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace AppWebLogisticaMoreno.Models.Entidades
+{
+    public class MOrdenCompra
+    {
+        [Key]
+        public int idOrdenCompra { get; set; }
+
+        [Display(Name = "Proveedor")]
+        [Required(ErrorMessage = "Debe de registrar el nombre del proveedor")]
+        [MaxLength(25, ErrorMessage = "El campo no debe de tener mas de 25 caracteres")]
+        public string Proveedor { get; set; }
+
+        [Display(Name = "Descripcion")]
+        [Required(ErrorMessage = "Debe de ingresar la descripcion del producto")]
+        [MaxLength(35, ErrorMessage = "El campo no debe de tener mas de 35 caracteres")]
+        public string Descripcion { get; set; }
+
+        [Display(Name = "UnidadMedida")]
+        [Required(ErrorMessage = "Debe de ingresar la UnidadMedida")]
+        [MaxLength(8, ErrorMessage = "El campo no debe de tener mas de 8 caracteres")]
+        public string UnidadMedida { get; set; }
+
+        public int Cantidad { get; set; }
+
+        public int Precio { get; set; }
+
+    }
+}
+
